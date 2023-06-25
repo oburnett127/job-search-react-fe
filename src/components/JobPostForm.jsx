9d42c11edc3a83850d36ea0cd9c6a0d76e3e16d3
Owen Burnett <oburnett127@gmail.com>
2023-06-25 03:11:07 -0500

    JobPostForm and JobUpdatForm have been modified to use react hook form.

--- src/components/JobPostForm.jsx
+++ src/components/JobPostForm.jsx
@@ -1,18 +1,19 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { Form, useNavigate, useNavigation, useActionData } from 'react-router-dom';
+import { useNavigate, useNavigation } from 'react-router-dom';
 import axios from 'axios';
 import classes from './JobPostForm.module.css';
 import { UserContext } from './UserContext';
+import { useForm } from 'react-hook-form';
 
 function JobPostForm({ method }) {
-  const data = useActionData();
   const navigate = useNavigate();
   const navigation = useNavigation();
   const { user } = useContext(UserContext);
   const isSubmitting = navigation.state === 'submitting';
-  const [formData, setFormData] = useState({ title: '', description: '' });
   const [employer, setEmployer] = useState(null);
   const [message, setMessage] = useState(null);
+
+  const {register, handleSubmit, formState: {errors}} = useForm();
 
   useEffect(() => {
     console.log("user's email " + user.email);
@@ -34,20 +35,13 @@ function JobPostForm({ method }) {
       }));
   }, [user.email]);
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
+  const onSubmit = (data) => {
     console.log("inside handleSubmit: " + employer?.id);
 
     const requestOptions = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ title: formData.title, employerId: employer?.id, description: formData.description})
+      body: JSON.stringify({ title: data.title, employerId: employer?.id, description: data.description})
     };
   
     fetch('http://localhost:8080/job/create', requestOptions)
@@ -76,45 +70,24 @@ function JobPostForm({ method }) {
   return (
     <>
       <p className={classes.message}>{message}</p>
-      <Form method={method} className={classes.form}>
-        {data && data['errors'] && (
-          <ul>
-            {Object.values(data['errors'].map((err) => (
-              <li key={err}>{err}</li>
-            )))}
-          </ul>
-        )}
+      <form method={method} className={classes.form} errors={errors} onSubmit={handleSubmit(onSubmit)}>
         <p>
           <label htmlFor="title">Title</label>
-          <input
-            id="title"
-            type="text"
-            name="title"
-            value={formData.title}
-            required
-            onChange={handleInputChange}
-          />
+          <input type="text" {...register("title", {required: true})} />
         </p>
+        {errors?.title && <span>The title is required.</span>}
         <p>
           <label htmlFor="description">Description</label>
-          <textarea
-            id="description"
-            name="description"
-            value={formData.description}
-            rows={5}
-            required
-            onChange={handleInputChange}
-          />
+          <textarea {...register("description", {required: true})} />
         </p>
+        {errors?.description && <span>The description is required.</span>}
         <div className={classes.actions}>
           <button type="button" onClick={handleCancel} disabled={isSubmitting}>
             Cancel
           </button>
-          <button disabled={isSubmitting} onClick={handleSubmit}>
-            {isSubmitting ? 'Submitting...' : 'Save'}
-          </button>
+          <button type="submit">Submit</button>
         </div>
-      </Form>
+      </form>
     </>
   );
 }
--- src/components/JobUpdateForm.jsx
+++ src/components/JobUpdateForm.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import { useState } from "react";
+import React, {useState} from 'react';
+import { useNavigate, useNavigation } from 'react-router-dom';
 import { useMutation, useQueryClient } from "react-query";
 import axios from "axios";
+import { useForm } from 'react-hook-form';
+import classes from './JobUpdateForm.module.css';
 
 const JobUpdateForm = ({ job }) => {
+    const navigate = useNavigate();
+    const navigation = useNavigation();
     const queryClient = useQueryClient();
-    const [title, setTitle] = useState(job.title);
-    const [description, setDescription] = useState(job.description);
+    const isSubmitting = navigation.state === 'submitting';
+    const [message, setMessage] = useState(null);
 
-    const { mutate, isLoading } = useMutation(
+    const {register, handleSubmit, formState: {errors}} = useForm();
+
+    const { mutate } = useMutation(
         (updatedJob) => axios.post(`http://localhost:8080/job/update/${job.id}`, updatedJob),
         {
             onSuccess: () => {
@@ -17,34 +23,44 @@ const JobUpdateForm = ({ job }) => {
         }
     );
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        mutate({ title, description });
+    const onSubmit = (data) => {
+        const title = data.title;
+        const description = data.description;
+        
+        try {
+            mutate({ title, description });
+        } catch(error) {
+            setMessage('Job update failed.');
+            return;
+        }
+
+        setMessage("Job update was successful.");
     };
 
+    const handleCancel = () => {
+        navigate('..');
+      }
+
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="title">Title:</label>
-                <input
-                    id="title"
-                    type="text"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                />
-            </div>
-            <div>
-                <label htmlFor="description">Description:</label>
-                <textarea
-                    id="description"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                />
-            </div>
-            <button type="submit" disabled={isLoading}>
-                {isLoading ? "Updating..." : "Update"}
-            </button>
-        </form>
+        <>
+            <p className={classes.message}>{message}</p>
+            <form errors={errors} onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <label htmlFor="title">Title:</label>
+                    <input type="text" {...register("title", {required: true})} />
+                </div>
+                {errors?.title && <span>The title is required.</span>}
+                <div>
+                    <label htmlFor="description">Description:</label>
+                    <textarea {...register("description", {required: true})} />
+                </div>
+                {errors?.description && <span>The description is required.</span>}
+                <button type="button" onClick={handleCancel} disabled={isSubmitting}>
+                    Cancel
+                </button>
+                <button type="submit">Submit</button>
+            </form>
+        </>
     );
 };
 
